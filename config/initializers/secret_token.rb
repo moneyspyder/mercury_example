089558196f80434c6bbd7e9add3825abf3572252
Warren Sangster <warren@moneyspyder.co.uk>
2012-12-20 12:59:31 +0000

    Adding intial sites

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MercuryExample::Application.config.secret_token = 'dbcb22b3f94d8e65ec3c716dfd314a5f58a6ea74f0a3bba2dd08c09df63855941a9b8f09f5321e63c987f5150123870fb32efe2226f03ef87e169363aaf4993d'
